ee55fc9eed2019b1e4b0c0d45f3e4b94b04d4fd1
RousseAnnie <147371647+RousseAnnie@users.noreply.github.com>
2023-10-09 03:23:06 -0400

    Add files via upload

---
+++ ejercicio1/index.php
@@ -0,0 +1,153 @@
+<?php 
+include "cabecera.inc.php";
+?>
+   <section class="slider_section">
+      <div id="myCarousel" class="carousel slide banner-main" data-ride="carousel">
+         <div class="carousel-inner">
+            
+            <div class="carousel-item active">
+               <img class="first-slide" src="images/f1.jpg" alt="First slide">
+                  <div class="container">
+                     <div class="carousel-caption relative" data-aos="fade-right">
+                        <b> <font size="4" style="color:#FFE933"> Pionera en el Sistema Universitario Boliviano </font> </b>
+                        <br> <br>          
+                        <b> <font size="6" style="color:#ECEADD"> EXCELENCIA EN LA FORMACION PROFESIONAL </font> </b>
+                        <br>
+                     </div>
+                  </div>
+            </div>
+            
+            <div class="carousel-item">
+               <img class="first-slide" src="images/f2.jpg" alt="First slide">
+                  <div class="container">
+                     <div class="carousel-caption relative" data-aos="fade-right">
+                        <b> <font size="4" style="color:#FFE933"> Nuevos planes de estudio 2020 </font> </b>
+                        <br> <br>          
+                        <b> <font size="6" style="color:#ECEADD"> REUNION ACADEMICA DE LA CARRERA </font> </b>
+                        <br>
+                     </div>
+                  </div>
+            </div>
+
+            <div class="carousel-item">
+               <img class="first-slide" src="images/f1.jpg" alt="First slide">
+                  <div class="container">
+                     <div class="carousel-caption relative" data-aos="fade-right">
+                        <b> <font size="4" style="color:#FFE933"> Instituto de Investigación en Informática </font> </b>
+                        <br> <br>          
+                        <b> <font size="6" style="color:#ECEADD"> DESARROLLO DE SOFTWARE & TECNOLOGIA </font> </b>
+                        <br>
+                     </div>
+                  </div>
+            </div>
+         </div>
+         
+         <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+         </a>
+         
+         <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+         </a>
+      </div>
+   </section>
+
+   <!-- about -->
+   <div class="about">
+      <div class="container">         
+         <div class="row">
+            <div class="col-md-10 offset-md-1">
+               <div class="aboutheading">
+                  <h2> Carrera <strong class="black"> de </strong> Informática </h2>
+                  <span>Formar profesionales altamente calificados en ciencia y tecnología del área de la Informática para responder a 
+                        las necesidades de la sociedad.</span>
+               </div>
+            </div>
+         </div>
+         
+         <div class="row border">
+            
+            <div class="col-xl-7 col-lg-7 col-md-12 col-sm-12">
+               <div class="about-box">
+                  <h2> <strong class="black"> MISION </strong> </h2>
+                     Formar profesionales idóneos con calidad humana, ética, valores, excelencia científica, compromiso social, capacidad crítica y creativa para potenciar el desarrollo de la ciencia y la tecnología en el área de la Informática en concordancia con los requerimientos de la sociedad y sus instituciones, desempeñándose con éxito en el ámbito regional, nacional e internacional. <br> <br>
+                  <h2> <strong class="black"> VISION </strong> </h2>
+                     Ser la unidad académica líder a nivel nacional y un referente de alto nivel en la formación de profesionales del área de la Informática, que aporta a la región y el país no solo con sus graduados sino también con proyectos de investigación y extensión de alto impacto relacionados con ciencia y tecnología.                  
+               </div>
+            </div>
+            
+            <div class="col-xl-5 col-lg-5 col-md-12 col-sm-12">
+               <div class="about-box">
+                  <br> <br> <br> <figure> <center> <img src="images/fcpn.jpg" alt="img"/> </center></figure>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+      
+   <!-- end about -->
+   <!-- Investigacion -->
+   <div class="Library">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-10 offset-md-1">
+               <div class="titlepage">
+                  <h2> Instituto <strong class="black"> de </strong> Investigación </h2>
+                  <span>Fomentar, planificar, coordinar y realizar actividades de investigación teórica y/o aplicada en informática.
+                        Buscar el más amplio beneficio de su labor investigativa a través de la colaboración e intercambio con otras instituciones del país y del extranjero.</span> 
+               </div>
+            </div>
+         </div>
+      </div>
+      
+      <figure> <center> <img src="images/inst.jpg" alt="img"/> </center> </figure>
+
+   </div>
+   
+   <!-- end Investigacion -->
+   <!--Licenciaturas -->
+   <div class="Books">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-10 offset-md-1">
+               <div class="titlepage">
+                  <h2> Kardex <strong class="black"> Académico </strong> de <strong class="black"> Informática </strong> </h2>
+               </div>
+            </div>
+         </div>
+         
+         <div class="row box">
+            <div class="col-xl-7 col-lg-7 col-md-12 col-sm-12">
+               <div class="about-box">
+                  <h2> <strong class="black"> DIRECCIÓN </strong> </h2>
+                     Av. Villazon Nro 1995 <br>
+                     Monoblock Central - Segundo Patio <br>
+                     Edif. Carrera de Informática - Piso 2 <br>
+                     Teléfono: +591 (2) 261-2917 <br>
+                     Email: kardex@informatica.edu.bo <br> <br>
+                  <h2> <strong class="black"> ATENCIÓN Y CONSULTAS </strong> </h2>
+                     Telf. WhatsApp: 67043390 – 76241931 <br>
+                     HORARIO DE ATENCIÓN: <br>
+                     HRS. 8:00 a 14:00 (LUNES A VIERNES)
+               </div>
+            </div>
+
+            <div class="col-xl-5 col-lg-5 col-md-12 col-sm-12">
+               <div class="ui feed segment">
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2274.618292150087!2d-68.13027117583569!3d-16.504034815012442!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7efa3457461b1e6a!2sCarrera+de+Inform%C3%A1tica+-+UMSA!5e0!3m2!1ses-419!2sbo!4v1550576981909" width="100%" height="240" frameborder="0" style="border:0" allowfullscreen></iframe>
+                  <div class="content">
+                     <p class="description" align="justify"> El Kardex Académico se encuentra en el 2do patio del Monoblock, edif. de Informática, Piso 2, cerca a la entrada de la calle Federico Zuazo.</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+
+         </div>
+      </div>
+      <!-- end Licenciatura -->
+    
+<?php 
+include "piedepagina.inc.php";
+?>
---
+++ ejercicio1/piedepagina.inc.php
@@ -0,0 +1,39 @@
+      <!-- footer -->
+      <footer>
+         <div class="footer">
+            <div class="container">
+               <div class="row pdn-top-30">
+                  <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                     
+                     <div class="Follow">
+                        <h3> Siguenos </h3>
+                     </div>
+                     
+                     <ul class="location_icon">
+                        <li> <a href="https://www.facebook.com/UMSAinformacion"><img src="icon/face.jpg"></a></li>
+                        <li> <a href="https://www.instagram.com/umsainformacion"><img src="icon/insta.jpg"></a></li>
+                        <li> <a href="https://twitter.com/UMSAinformacion"><img src="icon/x.jpg"></a></li>
+                        <li> <a href="https://www.tiktok.com/@umsa.bo"><img src="icon/tik.jpg"></a></li>
+                        <li> <a href="https://www.youtube.com/@UMSAinformacion"><img src="icon/yt.jpg"></a></li>
+                     </ul>
+
+                  </div>
+               </div>
+            </div>
+         </div>
+
+         <div class="copyright">
+            <div class="container">
+               <p> <b> Copyright 2023 <br> Derechos reservados por Rocio Andrea Calderon Marquez <br> INF-324 </b> </p>
+            </div>
+         </div>
+
+      </footer>
+
+      <?php
+         include "scripts.inc.php"; 
+      ?>
+   
+   </body>
+
+</html>
